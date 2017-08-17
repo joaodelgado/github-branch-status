2f2734a4e578733973fcea358962510c416fc832
João Delgado <joaomtdelgado@gmail.com>
2017-08-17 21:53:35 +0100

    Fix invalid link for undefined URLs

--- src/badge.js
+++ src/badge.js
@@ -51,8 +51,9 @@ class Badge {
     }
 
     success() {
+        const href = this.url ? `href="${this.url}"` : '';
         return `
-            <a class="text-green tooltipped tooltipped-e" aria-label="Success: This commit looks good" href="${this.url}">
+            <a class="text-green tooltipped tooltipped-e" aria-label="Success: This commit looks good" ${href}>
                 <svg aria-hidden="true" class="octicon octicon-check" height="16" version="1.1" viewBox="0 0 12 16" width="12">
                     <path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5z"></path>
                 </svg>
@@ -60,8 +61,9 @@ class Badge {
     }
 
     pending() {
+        const href = this.url ? `href="${this.url}"` : '';
         return `
-            <a class="bg-pending tooltipped tooltipped-e" aria-label="Pending: This commit is being built" href="${this.url}">
+            <a class="bg-pending tooltipped tooltipped-e" aria-label="Pending: This commit is being built" ${href}>
                 <svg aria-hidden="true" class="octicon octicon-primitive-dot" height="16" version="1.1" viewBox="0 0 8 16" width="8">
                     <path fill-rule="evenodd" d="M0 8c0-2.2 1.8-4 4-4s4 1.8 4 4-1.8 4-4 4-4-1.8-4-4z"></path>
                 </svg>
@@ -70,8 +72,9 @@ class Badge {
 
 
     failure() {
+        const href = this.url ? `href="${this.url}"` : '';
         return `
-            <a class="text-red tooltipped tooltipped-e" aria-label="Failure: This commit cannot be built" href="${this.url}">
+            <a class="text-red tooltipped tooltipped-e" aria-label="Failure: This commit cannot be built" ${href}>
                 <svg aria-hidden="true" class="octicon octicon-x" height="16" version="1.1" viewBox="0 0 12 16" width="12">
                     <path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"></path>
                 </svg>
