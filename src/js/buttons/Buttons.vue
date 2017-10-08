19148ce511661bcd80c23f0620bcc451b01dafd3
João Delgado <joaomtdelgado@gmail.com>
2017-10-08 16:05:51 +0100

    Only show RemoveBadgeButton when there is stuff to remove

--- src/js/buttons/Buttons.vue
+++ src/js/buttons/Buttons.vue
@@ -3,7 +3,7 @@
     <span>
         <token-button v-if="!this.store.token"></token-button>
         <add-badge-button v-if="this.store.token"></add-badge-button>
-        <remove-badge-button v-if="this.store.token"></remove-badge-button>
+        <remove-badge-button v-if="hasChecks"></remove-badge-button>
     </span>
 
 </template>
@@ -16,6 +16,19 @@ import './AddBadgeButton.vue';
 import './RemoveBadgeButton.vue';
 
 export default Vue.component('conf-buttons', {
+
+    computed: {
+        hasChecks() {
+            const { config } = this.store;
+            const owner = this.github.owner();
+            const repo = this.github.repo();
+
+            return config
+                && config[owner]
+                && config[owner][repo]
+                && config[owner][repo].length > 0;
+        },
+    },
 
 });
 
