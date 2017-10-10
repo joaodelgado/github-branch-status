65d66c2bafb33c39c0b9714487dbf6aae84a9dea
João Delgado <joaomtdelgado@gmail.com>
2017-10-10 22:45:46 +0100

    Bump version to 0.1.1

--- utils/generate-manifest.js
+++ utils/generate-manifest.js
@@ -1,11 +1,13 @@
 const fileSystem = require('fs-extra');
 const path = require('path');
 
+const pjson = require('../package.json');
 const manifest = require('../src/manifest.json');
 
-// generates the manifest file using the package.json informations
-manifest.description = process.env.npm_package_description;
-manifest.version = process.env.npm_package_version;
+// generates the manifest file using the package.json information
+manifest.name = pjson.name;
+manifest.description = pjson.description;
+manifest.version = pjson.version;
 
 fileSystem.writeFileSync(
     path.join(__dirname, '../build/manifest.json'),
